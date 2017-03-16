7ed332f901dc326d10db838ba76e1a56277ed1d8
Ildar.Urazbakhtin <programmerildarik@gmail.com>
2017-03-16 17:38:29 +0300

    Add Photo Exeption

--- products/admin.py
+++ products/admin.py
@@ -4,12 +4,15 @@ from products.models import Product, ProductImage
 
 #Запись в админку
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Product._meta.fields]
 
+
+
     class Meta:
         model = Product
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 
 
 class ProductImageAdmin(admin.ModelAdmin):
