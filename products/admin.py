f1c9bf41577578137df7ea30d185579a6d5fd211
Ildar.Urazbakhtin <programmerildarik@gmail.com>
2017-03-17 16:10:37 +0300

    admin. add Photos in Admin

--- products/admin.py
+++ products/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
-
 from products.models import Product, ProductImage
+
+class ProductImageline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
 
 #Запись в админку
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Product._meta.fields]
-
-
+    inlines = [ProductImageline]
 
     class Meta:
         model = Product
 admin.site.register(Product,ProductAdmin)
-
 
 class ProductImageAdmin(admin.ModelAdmin):
     list_display = [field.name for field in ProductImage._meta.fields]
